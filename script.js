d49ef798d90c50e676875f628197c150c3fdfa8c
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-12-14 17:30:06 -0500

    added buttons to book elements

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const bookContainer = document.querySelector('.books-container');
+const bookForm = document.querySelector('.book-form');
+
 // book constructor
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,35 +22,61 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
-// creates a HTML element from a book obj
-function bookElement(book) {
+function deleteBook(e) {
+  console.log(e.target);
+}
+
+function toggleRead(e) {
+  console.log(e);
+}
+
+// creates a HTML element for a book obj
+function bookElement(book, index) {
   let bookElement = document.createElement('div');
   bookElement.classList.add('book');
+  bookElement.setAttribute('data-index-number', index);
 
   let list = document.createElement('ul');
   bookElement.appendChild(list);
 
+  let delButton = document.createElement('button');
+  delButton.classList.add('delete-book');
+  delButton.addEventListener('click', deleteBook);
+  delButton.innerHTML = ('delete');
+
+  let readButton = document.createElement('button');
+  readButton.classList.add('toggle-read');
+  readButton.addEventListener('click', toggleRead);
+  readButton.innerHTML = ('toggle read');
+
+  let buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('button-container');
+  buttonContainer.appendChild(readButton);
+  buttonContainer.appendChild(delButton);
+  bookElement.appendChild(buttonContainer);
+
   for (key in book) {
     if (book.hasOwnProperty(key)) {
       let info = document.createElement('li');
-      info.innerHTML = book[key];
+      if (key === 'pages') {
+        info.innerHTML = `${book[key]} pages`;
+      } else {
+        info.innerHTML = book[key];
+      }
       list.appendChild(info);
     }
   }
   return bookElement; 
 }
 
-addBookToLibrary('Microserfs', 'Douglas Couplnad', 371, true);
-addBookToLibrary('Jicroserfs', 'Fouglas Fouplnad', 371, true);
-addBookToLibrary('Picroserfs', 'Bouglas Mouplnad', 371, true);
-addBookToLibrary('Gicroserfs', 'Oouglas Souplnad', 371, true);
-addBookToLibrary('Sicroserfs', 'Pouglas Douplnad', 371, true);
+addBookToLibrary('Microserfs', 'Douglas Coupland', 371, true);
+addBookToLibrary('Jicroserfs', 'Fouglas Foupland', 371, true);
+addBookToLibrary('Picroserfs', 'Bouglas Moupland', 371, true);
+addBookToLibrary('Gicroserfs', 'Oouglas Soupland', 371, true);
+addBookToLibrary('Sicroserfs', 'Pouglas Doupland', 371, true);
 
-const bookContainer = document.querySelector('.books-container');
-const bookForm = document.querySelector('.book-form');
-
-myLibrary.forEach(book => {
-  bookContainer.appendChild(bookElement(book));
+myLibrary.forEach((book, index) => {
+  bookContainer.appendChild(bookElement(book, index));
 });
 
 // makes new book from form values and appends it to the book container
@@ -60,5 +89,6 @@ function saveBook() {
   );
   addBookToLibrary(...book);
   bookForm.reset();
-  bookContainer.appendChild(bookElement(myLibrary[myLibrary.length -1]));
+  bookContainer.appendChild(bookElement(myLibrary[myLibrary.length -1], myLibrary.length -1));
 }
+
